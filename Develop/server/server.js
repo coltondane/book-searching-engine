f064cbb84dc1677fb44b7ef8dacc6057e58d2ba3
coltondane <113216904+coltondane@users.noreply.github.com>
2023-12-01 13:49:16 -0700

    restructured server.js to accommodate Apollo and graphql

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// import apollo server instead of routes
+const {  ApolloServer } = require('apollo-server-express');
 
-const app = express();
+// import typeDefs and resolvers
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+  
+  // if in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+
+};
+
+
